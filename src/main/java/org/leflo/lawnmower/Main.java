f7ef263b84f71ce035dd813bd7e485ac25ac7306
Florentin Leclercq <github@email.leclercq-florentin.fr>
2024-07-20 23:40:43 +0200

    Updated Main

--- src/main/java/org/leflo/lawnmower/Main.java
+++ src/main/java/org/leflo/lawnmower/Main.java
@@ -9,6 +9,12 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        if(args.length != 1){
+            LOGGER.severe("Usage: java -jar LawnMower.jar <file>");
+            System.exit(1);
+            return;
+        }
+
         try(LawnMowerProgram program = new LawnMowerProgram(new File(args[0]))){
             LawnMowerExecution execution = new LawnMowerExecution(program);
             execution.run();
