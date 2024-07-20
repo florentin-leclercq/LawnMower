dfced471793311277de718a60b4dd9e15895b025
Florentin Leclercq <github@email.leclercq-florentin.fr>
2024-07-20 23:34:44 +0200

    Added Main

---
+++ src/main/java/org/leflo/lawnmower/Main.java
@@ -0,0 +1,20 @@
+package org.leflo.lawnmower;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        try(LawnMowerProgram program = new LawnMowerProgram(new File(args[0]))){
+            LawnMowerExecution execution = new LawnMowerExecution(program);
+            execution.run();
+        } catch (IOException e) {
+            LOGGER.severe("An error occurred while reading the file: " + e.getMessage());
+        }
+    }
+
+}
